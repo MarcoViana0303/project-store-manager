43fec9c63c71e9665ce12b91771f4b0300acafb8
Marco Viana <marcoviana100@gmail.com>
2023-01-26 01:42:07 -0300

    feat: Requisito de testes parcialmente feitos 2.0

--- tests/unit/controllers/mocks/salesControllers.mock.js
+++ tests/unit/controllers/mocks/salesControllers.mock.js
@@ -11,25 +11,33 @@ const salesMock = [
     "productId": 3,
     "quantity": 15
   }
-];
+]
 
-const salesMockId = [
-  {
-    "date": "2023-01-25T23:24:22.000Z",
-    "productId": 2,
-    "quantity": 10
-  }
-];
+const salesById = {
+  "saleId": 1,
+  "date": "2023-01-25T23:24:22.000Z",
+  "productId": 2,
+  "quantity": 10
+};
 
-const createSale = [
-  {
-    productId: 1,
-    quantity: 5,
-  },
-];
+const returnGetById = {
+  type: null,
+  message: salesById,
+};
+
+const returnSales = {
+  id: 2,
+  itemsSold: [
+    {
+      productId: 3,
+      quantity: 15,
+    },
+  ],
+};
 
 module.exports = {
   salesMock,
-  salesMockId,
-  createSale,
-}
+  salesById,
+  returnGetById,
+  returnSales,
+};
--- tests/unit/controllers/sales.controllers.test.js
+++ tests/unit/controllers/sales.controllers.test.js
@@ -1,42 +1,92 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const salesMock = require('./mocks/salesControllers.mock');
+const { returnGetById, returnSales} = require('./mocks/salesControllers.mock');
 const salesControllers = require('../../../src/controllers/sales.controllers');
 chai.use(sinonChai);
 const salesServices = require('../../../src/services/sales.services');
 const { expect } = require('chai');
 
 describe('Testando a camada controllers', function () {
- 
-    it('Testando a rota /sales', async function () {
+  describe('Testes para o requisito 2', function () {
+    it('Testando a rota /sales com findAll', async function () {
       const req = {};
       const res = {};
 
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       // Arrange
-      sinon.stub(salesServices, 'findAllSales').resolves(salesMock.salesMock);
+      sinon.stub(salesServices, 'findAllSales').resolves({
+        type: null,
+        message: returnSales,
+      });
       // Act
       await salesControllers.findAllSales(req, res);
       // Assert
       expect(res.status).to.have.been.calledWith(200);
-      expect(res.json).to.been.calledWith(salesMock.salesMock);
+      expect(res.json).to.been.calledWith(returnSales);
     });
 
-    // it('Testando a rota /products/:id caso não encontre o id', async function () {
-    //   const req = { params: { id: 15 } };
-    //   const res = {};
+    it('Testando a rota /products/:id caso não encontre o id', async function () {
+      const req = { params: { id: 150000 } };
+      const res = {};
 
-    //   res.status = sinon.stub().returns(res);
-    //   res.json = sinon.stub().returns();
-    //   // Arrange
-    //   sinon.stub(productsServices, 'findProductById').resolves(false);
-    //   // Act
-    //   await productsControllers.findProductById(req, res);
-    //   // Assert
-    //   expect(res.status).to.been.calledWith(404);
-    //   expect(res.json).to.been.calledWithExactly({ message: 'Product not found' });
-    // })
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      // Arrange
+      sinon.stub(salesServices, 'findSaleById').resolves({
+        type: 404,
+        message: 'Sale not found',
+      });
+      // Act
+      await salesControllers.findSaleById(req, res);
+      // Assert
+      expect(res.status).calledWith(404);
+      expect(res.json).calledWith({ message: 'Sale not found' });
+    })
+
+    it('Testando a rota /products/:id caso encontre o id', async function () {
+      const req = { params: { id: 1 } };
+      const res = {};
+      // Arrange
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      // Act
+      sinon.stub(salesServices, 'findSaleById').resolves({
+        type: null,
+        message: 
+     [
+          {
+            date: '2023-01-14T12:25:35.000Z',
+            productId: 1,
+            quantity: 5,
+          },
+          {
+            date: '2023-01-14T12:25:35.000Z',
+            productId: 2,
+            quantity: 10,
+          },
+        ],
+      });
+
+      await salesControllers.findSaleById(req, res);
+      // Assert
+      expect(res.json).calledWith([
+        {
+          date: '2023-01-14T12:25:35.000Z',
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          date: '2023-01-14T12:25:35.000Z',
+          productId: 2,
+          quantity: 10,
+        },
+      ]);
+      expect(res.status).calledWith(200);
+    });
+
+  });
+
   afterEach(sinon.restore);
 });
